3f7335c305c3f8643dded5add1d260efbc1fc521
SubhamMohanty <subham087mohanty@gmail.com>
2023-02-12 12:33:43 +0530

    Change hello to Welcome To My

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class hello{
 public static void main(String[]args){
-System.out.println("Hello World");
+System.out.println("Welcome To My World");
 System.out.println("Subham Mohanty");
 }
 }
